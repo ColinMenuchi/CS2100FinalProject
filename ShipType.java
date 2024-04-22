5c62c1f0aab56b2e77f70d7e5c665858b9f1790e
Colin Menuchi <cdmenuchi@gmail.com>
2024-04-22 11:03:43 -0400

    Added Enumerated Type: ShipType

---
+++ ShipType.java
@@ -0,0 +1,14 @@
+/**
+ * Used to represent the types of ships that both the player
+ * and computer can have on their boards. There are 5 types:
+ * Aircraft Carrier, Battleship, Cruiser, Submarine, and Destroyer.
+ */
+
+public enum ShipType 
+{
+    ST_AIRCRAFT_CARRIER,
+    ST_BATTLESHIP,
+    ST_CRUISER,
+    ST_SUB,
+    ST_DESTROYER
+}
